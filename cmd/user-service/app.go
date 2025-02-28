55f6c6e3b0e8d5056b4bbbb8d56d6ad24aea7e15
Kyle Bremont <kyle.bremont@wpengine.com>
2025-02-28 15:20:31 -0700

    initial database setup
    Adding database package and creating a database connection in the user-service app.

--- cmd/user-service/app.go
+++ cmd/user-service/app.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/kbremont/tattoo-app/api/proto/gen/go/tattooapp/v1/pbconnect"
 	coreapp "github.com/kbremont/tattoo-app/internal/app"
+	"github.com/kbremont/tattoo-app/internal/app/database"
 	"github.com/kbremont/tattoo-app/internal/app/server"
 	"github.com/kbremont/tattoo-app/internal/pkg/config"
 )
 
 type app struct {
-	server *server.Server
+	server   *server.Server
+	database *sql.DB
 }
 
 func (a *app) start(ctx context.Context) error {
@@ -35,13 +38,24 @@ func (a *app) shutdown(ctx context.Context) {
 func newApp(ctx context.Context, cfg *config.Config) (*app, error) {
 	// TODO: setup logger
 
+	// setting up DB
+	maxPings := database.DefaultMaxPings
+	pingInterval := database.DefaultPingInterval
+
+	db, err := database.New(cfg.DatabaseURI, database.WithPingTimeout(maxPings, pingInterval))
+	if err != nil {
+		// TODO log error
+		return nil, err
+	}
+
+	// setting up server
 	srv, err := newServer(ctx, cfg)
 	if err != nil {
 		// TODO: log error
 		return nil, err
 	}
 
-	return &app{server: srv}, nil
+	return &app{database: db, server: srv}, nil
 }
 
 func newServer(ctx context.Context, cfg *config.Config) (*server.Server, error) {
