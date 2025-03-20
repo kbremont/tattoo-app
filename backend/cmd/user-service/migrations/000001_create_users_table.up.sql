f43f3bcfe26d1a612592da13109e458ba518bf48
Kyle Bremont <kyle.bremont@wpengine.com>
2025-03-20 17:44:52 -0600

    update migrations
    Update the migrations for the user-service and auth-service to include the new tables and columns required for the authentication and authorization of users.

--- backend/cmd/user-service/migrations/000001_create_users_table.up.sql
+++ backend/cmd/user-service/migrations/000001_create_users_table.up.sql
@@ -1,12 +1,11 @@
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS users (
-    id          SERIAL PRIMARY KEY,
-    email       TEXT,
-    -- password    TEXT,
-    first_name  TEXT,
-    last_name   TEXT,
-    created_on  TIMESTAMP DEFAULT NOW()
+    id UUID PRIMARY KEY, -- provided externally by Auth Service
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
 COMMIT;
