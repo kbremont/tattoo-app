510b43a8083612dd8112e8d4e10a0595ee22fa83
Kyle Bremont <kyle.bremont@wpengine.com>
2025-03-21 16:05:30 -0600

    add logic for handling tokens in the frontend
    Adding a provider for the token manager and updating the repository to save the tokens after signing up.

--- frontend/tattooapp/lib/src/features/auth/auth_providers.dart
+++ frontend/tattooapp/lib/src/features/auth/auth_providers.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:tattooapp/src/features/auth/data/auth_repository.dart';
+import 'package:tattooapp/src/core/services/token_manager.dart';
 import 'package:tattooapp/src/features/auth/application/sign_up_use_case.dart';
 import 'package:tattooapp/src/features/auth/application/log_in_use_case.dart';
 import 'package:tattooapp/src/features/auth/domain/auth_service_client.dart';
 
 // Service client factory
-final authServiceClientFactoryProvider = Provider((ref) => AuthServiceClientFactory());
+final authServiceClientFactoryProvider = Provider(
+  (ref) => AuthServiceClientFactory(),
+);
+
+// Secure storage
+final secureStorageProvider = Provider((ref) => FlutterSecureStorage());
 
 // Service client
 final authServiceClientProvider = Provider((ref) {
@@ -13,10 +20,18 @@ final authServiceClientProvider = Provider((ref) {
   return factory.createClient();
 });
 
+// Token manager
+final tokenManagerProvider = Provider((ref) {
+  final storage = ref.watch(secureStorageProvider);
+  final client = ref.watch(authServiceClientProvider);
+  return TokenManager(storage, client);
+});
+
 // Repository
 final authRepositoryProvider = Provider((ref) {
   final client = ref.watch(authServiceClientProvider);
-  return AuthRepository(client);
+  final tokenManager = ref.watch(tokenManagerProvider);
+  return AuthRepository(client, tokenManager);
 });
 
 // Use cases
