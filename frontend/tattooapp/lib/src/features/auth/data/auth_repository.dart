97442a6e25cbbc7b488e6d8e298721f75975bdb0
Kyle Bremont <kyle.bremont@wpengine.com>
2025-03-21 14:44:31 -0600

    update frontend to pass first and last name
    Updating flutter client to pass first and last name to the backend when signing up a new user.

--- frontend/tattooapp/lib/src/features/auth/data/auth_repository.dart
+++ frontend/tattooapp/lib/src/features/auth/data/auth_repository.dart
@@ -7,37 +7,34 @@ class AuthRepository {
 
   AuthRepository(this._client);
 
-  Future<SignUpResponse> signUp(String email, String password) {
+  Future<SignUpResponse> signUp(
+    String firstName,
+    String lastName,
+    String email,
+    String password,
+  ) {
     final request = SignUpRequest(
-      credentials: UserCredentials(
-        email: email,
-        password: password,
-      ),
+      credentials: UserCredentials(email: email, password: password),
+      firstName: firstName,
+      lastName: lastName,
     );
     return _client.signUp(request);
   }
 
   Future<LogInResponse> logIn(String email, String password) {
     final request = LogInRequest(
-      credentials: UserCredentials(
-        email: email,
-        password: password,
-      ),
+      credentials: UserCredentials(email: email, password: password),
     );
     return _client.logIn(request);
   }
 
   Future<RefreshTokenResponse> refreshToken(String refreshToken) {
-    final request = RefreshTokenRequest(
-      refreshToken: refreshToken,
-    );
+    final request = RefreshTokenRequest(refreshToken: refreshToken);
     return _client.refreshToken(request);
   }
 
   Future<LogOutResponse> logOut(String refreshToken) {
-    final request = LogOutRequest(
-      refreshToken: refreshToken,
-    );
+    final request = LogOutRequest(refreshToken: refreshToken);
     return _client.logOut(request);
   }
 }
