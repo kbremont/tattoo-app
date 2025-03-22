7be317ba129fb811b2ae99bc37313736b6deb581
Kyle Bremont <kyle.bremont@wpengine.com>
2025-03-22 15:05:28 -0600

    use new logger in user service

--- backend/internal/app/user_service.go
+++ backend/internal/app/user_service.go
@@ -2,18 +2,19 @@ package app
 
 import (
 	"context"
-	"log"
 
 	"connectrpc.com/connect"
 	"github.com/google/uuid"
 	v1pb "github.com/kbremont/tattoo-app/api/proto/gen/go/tattooapp/v1"
 	"github.com/kbremont/tattoo-app/api/proto/gen/go/tattooapp/v1/pbconnect"
+	"github.com/kbremont/tattoo-app/backend/internal/pkg/log"
 	"github.com/kbremont/tattoo-app/backend/internal/pkg/models"
 )
 
 // UserService is the implementation of tattooapp.v1.UserService.
 type UserService struct {
 	repository UserRepository
+	logger     log.Logger
 }
 
 // UserRepository is the interface that allows stateful operations on a users.
@@ -26,25 +27,27 @@ type UserRepository interface {
 
 var _ pbconnect.UserServiceHandler = new(UserService)
 
-func NewUserService(repo UserRepository) *UserService {
-	return &UserService{repository: repo}
+func NewUserService(l log.Logger, repo UserRepository) *UserService {
+	return &UserService{logger: l, repository: repo}
 }
 
 // CreateUser implements tattooapp.v1.UserService.CreateUser.
 func (s *UserService) CreateUser(ctx context.Context, req *connect.Request[v1pb.CreateUserRequest]) (*connect.Response[v1pb.CreateUserResponse], error) {
-	log.Println("Creating user: ", req.Msg.GetFirstName())
+	logger := log.FromContext(ctx)
+	logger.Info(ctx, "handling CreateUser request", "first_name", req.Msg.GetFirstName(), "last_name", req.Msg.GetLastName())
 
-	id, _ := uuid.Parse(req.Msg.GetId())
 	u := &models.User{
-		ID:        id,
+		ID:        uuid.New(),
 		FirstName: req.Msg.GetFirstName(),
 		LastName:  req.Msg.GetLastName(),
 	}
 
 	if err := s.repository.CreateUser(ctx, u); err != nil {
+		logger.WithError(err).Error(ctx, "failed to create user", "first_name", req.Msg.GetFirstName(), "last_name", req.Msg.GetLastName())
 		return nil, err
 	}
 
+	logger.Info(ctx, "user created successfully", "user_id", u.ID.String())
 	return connect.NewResponse(&v1pb.CreateUserResponse{User: &v1pb.User{
 		Id:        u.ID.String(),
 		FirstName: u.FirstName,
@@ -54,14 +57,17 @@ func (s *UserService) CreateUser(ctx context.Context, req *connect.Request[v1pb.
 
 // GetUser implements tattooapp.v1.UserService.GetUser.
 func (s *UserService) GetUser(ctx context.Context, req *connect.Request[v1pb.GetUserRequest]) (*connect.Response[v1pb.GetUserResponse], error) {
-	log.Println("Getting user: ", req.Msg.GetId())
+	logger := log.FromContext(ctx)
+	logger.Info(ctx, "handling GetUser request", "user_id", req.Msg.GetId())
 
 	id, _ := uuid.Parse(req.Msg.GetId())
 	u, err := s.repository.GetUser(ctx, id)
 	if err != nil {
+		logger.WithError(err).Error(ctx, "failed to get user", "user_id", req.Msg.GetId())
 		return nil, err
 	}
 
+	logger.Info(ctx, "user retrieved successfully", "user_id", u.ID.String())
 	return connect.NewResponse(&v1pb.GetUserResponse{User: &v1pb.User{
 		Id:        u.ID.String(),
 		FirstName: u.FirstName,
@@ -71,7 +77,8 @@ func (s *UserService) GetUser(ctx context.Context, req *connect.Request[v1pb.Get
 
 // UpdateUser implements tattooapp.v1.UserService.UpdateUser.
 func (s *UserService) UpdateUser(ctx context.Context, req *connect.Request[v1pb.UpdateUserRequest]) (*connect.Response[v1pb.UpdateUserResponse], error) {
-	log.Println("Updating user: ", req.Msg.GetId())
+	logger := log.FromContext(ctx)
+	logger.Info(ctx, "handling UpdateUser request", "user_id", req.Msg.GetId())
 
 	id, _ := uuid.Parse(req.Msg.GetId())
 	u := &models.User{
@@ -81,9 +88,11 @@ func (s *UserService) UpdateUser(ctx context.Context, req *connect.Request[v1pb.
 	}
 
 	if err := s.repository.UpdateUser(ctx, u); err != nil {
+		logger.WithError(err).Error(ctx, "failed to update user", "user_id", req.Msg.GetId())
 		return nil, err
 	}
 
+	logger.Info(ctx, "user updated successfully", "user_id", u.ID.String())
 	return connect.NewResponse(&v1pb.UpdateUserResponse{User: &v1pb.User{
 		Id:        u.ID.String(),
 		FirstName: u.FirstName,
@@ -93,12 +102,15 @@ func (s *UserService) UpdateUser(ctx context.Context, req *connect.Request[v1pb.
 
 // DeleteUser implements tattooapp.v1.UserService.DeleteUser.
 func (s *UserService) DeleteUser(ctx context.Context, req *connect.Request[v1pb.DeleteUserRequest]) (*connect.Response[v1pb.DeleteUserResponse], error) {
-	log.Println("Deleting user: ", req.Msg.GetId())
+	logger := log.FromContext(ctx)
+	logger.Info(ctx, "handling DeleteUser request", "user_id", req.Msg.GetId())
 
 	id, _ := uuid.Parse(req.Msg.GetId())
 	if err := s.repository.DeleteUser(ctx, id); err != nil {
+		logger.WithError(err).Error(ctx, "failed to delete user", "user_id", req.Msg.GetId())
 		return nil, err
 	}
 
+	logger.Info(ctx, "user deleted successfully", "user_id", id.String())
 	return connect.NewResponse(&v1pb.DeleteUserResponse{Success: true}), nil
 }
