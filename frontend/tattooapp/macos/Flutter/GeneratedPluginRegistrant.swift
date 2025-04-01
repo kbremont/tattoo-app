094fe75fcc6296a146b0b585daf7423859333130
Kyle Bremont <kyle.bremont@wpengine.com>
2025-04-01 15:08:12 -0600

    update pubspec.yaml
    Updating `pubspec.yaml` with required dependencies for the new features. Also adding assests to be used with Lottie.

--- frontend/tattooapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/tattooapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import auth0_flutter
-import flutter_secure_storage_macos
-import path_provider_foundation
+import device_info_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
