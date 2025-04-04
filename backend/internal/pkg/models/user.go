60ef124f41b0eaa2a216f6a9a3edd99700eb4dd1
Kyle Bremont <kyle.bremont@wpengine.com>
2025-04-03 19:57:09 -0600

    update user models
    Updating user models to add the new user role field.

--- backend/internal/pkg/models/user.go
+++ backend/internal/pkg/models/user.go
@@ -4,8 +4,16 @@ import (
 	"time"
 )
 
+type UserRole string
+
+const (
+	UserRoleArtist UserRole = "artist"
+	UserRoleClient UserRole = "client"
+)
+
 type User struct {
 	Auth0UserID string
+	Role        UserRole
 	FirstName   string
 	LastName    string
 	CreatedAt   time.Time
--- frontend/tattooapp/lib/src/features/user/domain/user.dart
+++ frontend/tattooapp/lib/src/features/user/domain/user.dart
@@ -1,31 +1,35 @@
+enum UserRole { artist, client }
+
 class User {
   final String auth0UserId;
+  final UserRole role;
   final String firstName;
   final String lastName;
   final List<String> stylePreferences;
 
   const User({
     required this.auth0UserId,
+    required this.role,
     required this.firstName,
     required this.lastName,
     required this.stylePreferences,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      auth0UserId: json['id'] as String,
-      firstName: json['firstName'] as String,
-      lastName: json['lastName'] as String,
-      stylePreferences: List<String>.from(json['stylePreferences'] ?? []),
-    );
-  }
+  // factory User.fromJson(Map<String, dynamic> json) {
+  //   return User(
+  //     auth0UserId: json['id'] as String,
+  //     firstName: json['firstName'] as String,
+  //     lastName: json['lastName'] as String,
+  //     stylePreferences: List<String>.from(json['stylePreferences'] ?? []),
+  //   );
+  // }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': auth0UserId,
-      'firstName': firstName,
-      'lastName': lastName,
-      'stylePreferences': stylePreferences,
-    };
-  }
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     'id': auth0UserId,
+  //     'firstName': firstName,
+  //     'lastName': lastName,
+  //     'stylePreferences': stylePreferences,
+  //   };
+  // }
 }
