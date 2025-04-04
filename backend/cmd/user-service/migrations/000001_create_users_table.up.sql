4bf0e3e31468dec0573668f869436303162767bf
Kyle Bremont <kyle.bremont@wpengine.com>
2025-04-03 19:58:26 -0600

    update user table migrations
    Updating user table migrations to handle user roles.

--- backend/cmd/user-service/migrations/000001_create_users_table.up.sql
+++ backend/cmd/user-service/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,13 @@
 BEGIN;
 
+CREATE TYPE user_role AS ENUM (
+    'artist',
+    'client'
+);
+
 CREATE TABLE IF NOT EXISTS users (
     auth0_user_id VARCHAR(255) PRIMARY KEY,
+    role user_role NOT NULL DEFAULT 'client',
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
