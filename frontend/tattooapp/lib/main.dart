36cb7d9f2e41cc1dd5b47f7bb70ef593ccd3c8f5
Kyle Bremont <kyle.bremont@wpengine.com>
2025-04-03 20:06:37 -0600

    set preferreed orientation of app

--- frontend/tattooapp/lib/main.dart
+++ frontend/tattooapp/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tattooapp/src/app/presentation/app_root.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const ProviderScope(child: AppRoot()));
 }
