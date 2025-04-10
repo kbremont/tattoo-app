fd781ee9fe55bb963a5e0a27b03155e166db1e6a
Kyle Bremont <kyle.bremont@wpengine.com>
2025-04-10 13:55:36 -0600

    update user service and repository to handle style preferences
    - Updating the user repository to handle style preferences in the database.
    - Updating the user service to handle style preferences in RPC functions.

--- backend/internal/app/user/user_repository.go
+++ backend/internal/app/user/user_repository.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 
 	"github.com/kbremont/tattoo-app/backend/internal/pkg/models"
+	"github.com/lib/pq"
 )
 
 // UserRepository is the interface that allows stateful operations on a users.
@@ -24,33 +25,44 @@ func NewUserRepository(db *sql.DB) *UserRepository { return &UserRepository{db: 
 
 func (r *UserRepository) CreateUser(ctx context.Context, u *models.User) error {
 	const exec = `INSERT INTO "users"
-  (id, role, first_name, last_name, created_at, updated_at)
-  VALUES ($1, $2, $3, $4, NOW(), NOW());`
+  (id, role, first_name, last_name, style_preferences, created_at, updated_at)
+  VALUES ($1, $2, $3, $4, $5, NOW(), NOW());`
 
-	_, err := r.db.ExecContext(ctx, exec, u.Id, u.Role, u.FirstName, u.LastName)
+	_, err := r.db.ExecContext(ctx, exec, u.Id, u.Role, u.FirstName, u.LastName, pq.Array(u.StylePreferences))
 	return err
 }
 
 func (r *UserRepository) GetUser(ctx context.Context, id string) (*models.User, error) {
-	const exec = `SELECT id, role, first_name, last_name, avatar_url, created_at, updated_at
+	var stylePrefs []string
+	const exec = `SELECT id, role, first_name, last_name, style_preferences, avatar_url, created_at, updated_at
   FROM "users"
   WHERE id = $1;`
 
 	var u models.User
-	err := r.db.QueryRowContext(ctx, exec, id).Scan(&u.Id, &u.Role, &u.FirstName, &u.LastName, &u.AvatarUrl, &u.CreatedAt, &u.UpdatedAt)
+	err := r.db.QueryRowContext(ctx, exec, id).Scan(&u.Id, &u.Role, &u.FirstName, &u.LastName, pq.Array(&stylePrefs), &u.AvatarUrl, &u.CreatedAt, &u.UpdatedAt)
 	if err != nil {
 		return nil, err
+	}
+
+	u.StylePreferences = make([]models.TattooStyle, len(stylePrefs))
+	for i, s := range stylePrefs {
+		u.StylePreferences[i] = models.TattooStyle(s)
 	}
 
 	return &u, nil
 }
 
 func (r *UserRepository) UpdateUser(ctx context.Context, u *models.User) error {
+	var stylePrefs []string
 	const exec = `UPDATE "users"
-  SET first_name = $1, last_name = $2, avatar_url = $3, updated_at = NOW()
-  WHERE id = $4;`
+  SET first_name = $1, last_name = $2, style_preferences = $3 avatar_url = $4, updated_at = NOW()
+  WHERE id = $5;`
 
-	_, err := r.db.ExecContext(ctx, exec, u.FirstName, u.LastName, u.AvatarUrl, u.Id)
+	_, err := r.db.ExecContext(ctx, exec, u.FirstName, u.LastName, pq.Array(stylePrefs), u.AvatarUrl, u.Id)
+	u.StylePreferences = make([]models.TattooStyle, len(stylePrefs))
+	for i, s := range stylePrefs {
+		u.StylePreferences[i] = models.TattooStyle(s)
+	}
 	return err
 }
 
